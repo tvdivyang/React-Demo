1d162326006898641e486aa4bb381cc0515ab8d8
tvdivyang <divyang@techvoot.com>
2023-02-08 18:53:03 +0530

    Animation

--- demo/src/App.js
+++ demo/src/App.js
@@ -2,10 +2,13 @@ import "./App.css";
 import React, { Component } from "react";
 import Count from "./Count";
 import UseRef from "./UseRef";
-import Tippy from '@tippyjs/react';
-import 'tippy.js/dist/tippy.css';
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
 import Protel from "./protel";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+import {motion} from "framer-motion";
+
+import {logo }from "./assets/index";
 
 export default class App extends Component {
   constructor() {
@@ -29,7 +32,6 @@ export default class App extends Component {
 
     return (
       <>
-
         <div>
           <h5>React Jsx</h5>
           hello {this.name}
@@ -68,23 +70,85 @@ export default class App extends Component {
         </div>
 
         <hr></hr>
-    <div>
-      <Tippy content="hello i am divyang">
-        <button>Tooltip</button>
-      </Tippy>
-          
+        <div>
+          <Tippy content="hello i am divyang">
+            <button>Tooltip</button>
+          </Tippy>
         </div>
         <hr />
+        <div id="myDIV">
+          <h6>css style </h6>
+        </div>
+        <hr/>
+        <div>
+          <h5>Animation</h5>
+          <motion.div animate={{scale:1.5,borderRadius:"25%",  transform: "rotate(20deg)"}} className="animation"></motion.div>
+          <motion.button animate={{rotate:360,scale:1.5}} className="m-5">Rotate</motion.button>
+        </div>
+        <hr/>
 
+        <div className="container" style={{background:"#eae3e369"}}  data-aos="flip-left">
+          <div className="row">
+            <div className="col-6 text-center" style={{background:"#6dcfee"}}>
+            <img src={logo} alt="img not found" className="mt-5"/>
+            </div>
+            <div className="col-6">
+              <h5>ANIMATION</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, suscipit.</p>
+              <button type="button" className="btn btn-primary">submit</button>
+            </div>
+          </div>
+        </div>
+        <hr/>
+
+        <div className="container" style={{background:"#eae3e369"}}  data-aos="slide-down">
+          <div className="row">
+            <div className="col-6 text-center" style={{background:"#6dcfee"}}>
+            <img src={logo} alt="img not found" className="mt-5"/>
+            </div>
+            <div className="col-6">
+              <h5>ANIMATION</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, suscipit.</p>
+              <button type="button" className="btn btn-primary">submit</button>
+            </div>
+          </div>
+        </div>
+        <hr/>
+
+        <div className="container" style={{background:"#eae3e369"}}  data-aos="zoom-in-up">
+          <div className="row">
+            <div className="col-6 text-center" style={{background:"#6dcfee"}}>
+            <img src={logo} alt="img not found" className="mt-5"/>
+            </div>
+            <div className="col-6">
+              <h5>ANIMATION</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, suscipit.</p>
+              <button type="button" className="btn btn-primary">submit</button>
+            </div>
+          </div>
+        </div>
+        <hr/>
+
+        <div className="container" style={{background:"#eae3e369"}}  data-aos="top-center">
+          <div className="row">
+            <div className="col-6 text-center" style={{background:"#6dcfee"}}>
+            <img src={logo} alt="img not found" className="mt-5"/>
+            </div>
+            <div className="col-6">
+              <h5>ANIMATION</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, suscipit.</p>
+              <button type="button" className="btn btn-primary">submit</button>
+            </div>
+          </div>
+        </div>
+        <hr/>
       </>
     );
   }
 }
 
-const root = ReactDOM.createRoot(document.getElementById('newRoot'));
-root.render(
- <Protel></Protel>
-);
+const root = ReactDOM.createRoot(document.getElementById("newRoot"));
+root.render(<Protel></Protel>);
 // const shoot = () => {
 //   document.getElementById("hy").innerHTML = "jay swaminarayan";
 // }
